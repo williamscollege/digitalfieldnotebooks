430be0779b5977c74e2b210e4525d2c676e2aa3b
Chris Warren <cwarren@williams.edu>
2014-11-21 16:12:49 -0500

    placeholder for user page so that people don't get broken links

---
+++ app_code/user.php
@@ -0,0 +1,9 @@
+<?php
+    require_once('../app_setup.php');
+	$pageTitle = ucfirst(util_lang('user'));
+	require_once('../app_head.php');
+
+echo "TODO: implement user page";
+
+require_once('../foot.php');
+?>
